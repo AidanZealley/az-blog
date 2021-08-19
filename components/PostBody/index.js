9a9d3ebe3544563b028e218a295d3f1dfb03c31b
Aidan Zealley <aidan@zealley.com>
2021-08-19 21:57:45 +0100

    put in fallback for aspect-ratio

--- components/PostBody/index.js
+++ components/PostBody/index.js
@@ -1,11 +1,14 @@
 import ReactMarkdown from "react-markdown";
+import { Container } from '@components/Container';
 import { CodeBlock } from '@components/CodeBlock';
 import styles from './styles.module.css';
 
 export const PostBody = ({ body }) => {
   return (
-    <div className={styles.body}>
-      <ReactMarkdown components={CodeBlock}>{body}</ReactMarkdown>
-    </div>
+    <Container size="content">
+      <div className={styles.body}>
+        <ReactMarkdown components={CodeBlock}>{body}</ReactMarkdown>
+      </div>
+    </Container>
   );
 };
